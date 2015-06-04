ff48b832d528f4f13674c4315d2bbc376aee4094
Ramona Bellamy <bellamy2727@yahoo.com>
2015-06-03 21:40:37 -0500

    imdb-app

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
 
+	var rowString = $('.movie-info').html();
+	var buildRow = _.template(rowString);
+
 	var App = Backbone.Router.extend({
 			routes:{
 				'': 					'home',
-				'search/:query':  		'search'
+				'search/:query':  		'search',
 			},
 
 			home: function(){
@@ -14,38 +17,45 @@ $(document).ready(function(){
 			},
 
 			search: function(query){
-				$('h1').html('Results');
-				$('h4').hide();
-				$('#form').show();
-				$('#search').show();
-				$.get( 'http://www.omdbapi.com/?', {s: query}, function(movies){
-					
-					var array = [];
-					 // var watch = [];
-
-
-					for(var i = 0; i< movies.Search.length; i++){
-						//console.log(movies.Search[i]);
-		
-						var  $movie = $('<div> '+movies.Search[i].Title+'</div>')				
-						array.push($movie);					
-					}		
-					//adds click event
-					for(var i = 0; i <array.length; i++){
-						array[i].click(function(e){	
-
-							$(this).css('opacity', '.5');
-							// watch.push($(this))
-							 // $('.watchlist').append(watch);
-						
-							 				
-					});
+				// $('h1').html('Results');
+					$('#form').show();
+					$('#search').show();
+					$.get( 'http://www.omdbapi.com/?', {s: query}, function(moviesData){
+			
+						 var movies = moviesData.Search
+						 $('.results').html('');
+						 //add search results
+						for(var i = 0; i< movies.length; i++){						
+							var movie =  movies[i];						
+							var addMovies = buildRow(movie);
+							$('.results').append(addMovies);								
+						}	
+						//add title to watchlist
+						$('.temp').each(function(){
+							$(this).click(function(e){
+								// console.log($('.watchlist').has('div').html())
+								$(this).parent().css('opacity', '.8');
+								var title = $(this).parent().find('.title').text()
+								var year = $(this).parent().find('.year').text()
+								$string = $('<div>'+ title +" "+ year +'</div>')
+								//console.log($('.watchlist').text());	
+								$('.watchlist').append($string.fadeIn('slow'));
+							});
+						});	
+						//remove title from watchlist
+						$('.watchlist').each(function(){
+							$(this).click(function(e){
+								 $(e.target).fadeOut('fast', function(){
+								 	$(this).remove();
+								 });
+								
+							})
+						})			
 					}
-						
-					$('.results').html(array);
+					,'json');
 				}
-				,'json');
-			}
+
+			
 	});
 
 		var myRouter = new App();
@@ -58,7 +68,7 @@ $(document).ready(function(){
 			myRouter.navigate('search/'+ query, {trigger: true});
 		});
 		
-
+	
 		
 
 });
